98e4c2aded8c0b74f0143a9fe64dd91f83aba3de
Sulinishka <46223205+Sulinishka@users.noreply.github.com>
2019-01-25 19:15:28 +0300

    Update stress_test.py

--- stress_test.py
+++ stress_test.py
@@ -2,9 +2,13 @@ import async_benchmark
 import unittest
 
 class TestStringMethods(unittest.TestCase):
-   
+    setup_done = False
+     
     def setUp(self):
-        self.benchmark = async_benchmark.Benchmark()
+        if not self.setup_done:    
+            self.__class__.benchmark = async_benchmark.Benchmark()
+            self.__class__.setup_done = True
+
     
     def test_80_percentile_delay(self):
         self.assertTrue(self.benchmark.percentile_80<450)
